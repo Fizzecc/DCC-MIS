a5dd0bc9a90677391dde0a6b5c1d5fd38a4492ae
FIzzecc <fizzecc@gmail.com>
2017-06-20 11:11:03 +0800

    Revert "asd"
    This reverts commit 205758490c4689c350811da9b7f64a861542d200.

--- DavaoChestCenter/formLogIn.cs
+++ DavaoChestCenter/formLogIn.cs
@@ -95,7 +95,6 @@ namespace DavaoChestCenter
 
     public class conClass
     {
-        // Dapat naay port!
         public static readonly string connectionString = "Server=localhost;Port=3307;Database=chest;Uid=root;Pwd=root;";
     }
 }
