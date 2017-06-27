bd5a34538cfa4a0bab95998c987a34ddb3bcf078
cevcrumb <cevcrumb@JF610B-005>
2017-06-27 11:14:39 +0800

    created registraiton form,

--- DavaoChestCenter/Program.cs
+++ DavaoChestCenter/Program.cs
@@ -16,7 +16,7 @@ namespace DavaoChestCenter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formLogIn());
+            Application.Run(new Registration());
         }
     }
 }
