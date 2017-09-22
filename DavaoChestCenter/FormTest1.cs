e25e5eb0fbb9eb9a6c624cf34d67ff54591675d1
alice crumb <alice crumb@CRUMB>
2017-09-22 14:59:20 +0800

    ehh

---
+++ DavaoChestCenter/FormTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DavaoChestCenter
+{
+    public partial class formTest1 : Form
+    {
+
+        int selectedid;
+        public formTest1()
+        {
+            InitializeComponent();
+        }
+
+        private void test1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedid = int.Parse(dataGridView1.Rows[e.RowIndex].Cells["id"].Value.ToString());
+            string firstname = dataGridView1.Rows[e.RowIndex].Cells["firstname"].Value.ToString();
+            string middlename = dataGridView1.Rows[e.RowIndex].Cells["middlename"].Value.ToString();
+            string lastname = dataGridView1.Rows[e.RowIndex].Cells["lastname"].Value.ToString();
+            string username = dataGridView1.Rows[e.RowIndex].Cells["username"].Value.ToString();
+            string password = dataGridView1.Rows[e.RowIndex].Cells["password"].Value.ToString();
+
+            var update = new formTest2(selectedid, firstname, middlename, lastname, username, password);
+            update.ref_To_Main = this;
+            update.ShowDialog();
+        }
+    }
+}
---
+++ DavaoChestCenter/formTest2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DavaoChestCenter
+{
+    public partial class formTest2 : Form
+    {
+        public formTest1 ref_To_Main { get; set; }
+
+        public formTest2()
+        {
+            InitializeComponent();
+        }
+
+        private void test2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
